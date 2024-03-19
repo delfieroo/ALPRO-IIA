d9720bd7b20a0141f88e8d6c3c867ac8d07736a9
lenovo loq <tuanakottadelfiero@gmail.com>
2024-03-19 17:52:12 +0900

    first commit

---
+++ KonversiSuhu.py
@@ -0,0 +1,10 @@
+aprint('Input suhu celsius: ')
+celsius = float(input())
+
+fahrenheit = (celsius * 1.8) + 32
+kelvin = celsius + 273.15
+reamur = celsius * (4/5)
+
+print(celsius,'derajat Celsius =',fahrenheit,'derajat Fahrenheit')
+print(celsius,'derajat Celsius =',kelvin,'derajat Kelvin')
+print(celsius,'derajat Celsius =',reamur,'derajat Reamur')
